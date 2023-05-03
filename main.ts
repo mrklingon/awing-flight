14f25da387f3e9d29ecd675de47f2dc111c4bc15
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-05-03 13:01:19 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,23 @@
+function Shift () {
+    for (let index = 0; index <= 3; index++) {
+        for (let I2 = 0; I2 <= 4; I2++) {
+            led.plotBrightness(index, I2, led.pointBrightness(index + 1, I2))
+        }
+    }
+}
 input.onButtonPressed(Button.A, function () {
     Aw += -1
     if (Aw < 0) {
         Aw = 0
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let I3 = 0; I3 <= 4; I3++) {
+        led.plot(I3, Aw)
+    }
+    basic.pause(100)
+    for (let I3 = 0; I3 <= 4; I3++) {
+        led.unplot(I3, Aw)
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -21,5 +37,6 @@ images.createBigImage(`
 basic.showString("A-Wing")
 Aw = 2
 basic.forever(function () {
+    Shift()
     led.plot(0, Aw)
 })
